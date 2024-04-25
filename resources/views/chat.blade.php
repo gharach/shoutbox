ecbcf866c5cc965736071787d295276d38c3f9c9
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2024-04-25 19:59:47 +0430

    Create chatroom view

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/chat.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="chatroom-container">
+        <div class="chatroom-header">shoutBox</div>
+        <div class="chatroom-body">
+        </div>
+        <div class="chatroom-footer">
+        </div>
+    </div>
+</div>
+@endsection
