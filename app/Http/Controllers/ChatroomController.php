6d961dfff5a4b4538ca43a8673a2bd62a7e678ac
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2024-04-25 21:24:59 +0430

    Implement send message functionality backend side in controller

--- app/Http/Controllers/ChatroomController.php
+++ app/Http/Controllers/ChatroomController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Events\MessageDelivered;
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatroomController extends Controller
 {
@@ -31,5 +32,23 @@ class ChatroomController extends Controller
     public function getMessages()
     {
         return Message::with('user')->latest()->take(2)->get();
+    }
+
+    /**
+     * Persist message to database
+     *
+     * @param  Request $request
+     * @return Response
+     */
+    public function sendMessage(Request $request)
+    {
+        $user = Auth::user();
+        $message = $user->messages()->create([
+            'message' => $request->input('message')
+        ]);
+
+
+
+        return ['status' => 'Message Sent!'];
     }
 }
