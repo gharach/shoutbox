706748c41686eebec81863b66c0cca754cfa98df
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2024-04-26 15:03:08 +0430

    Add delete outdated messages

---
+++ app/Console/Commands/DeleteOutdatedMessages.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DeleteOutdatedMessages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-outdated-messages';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
