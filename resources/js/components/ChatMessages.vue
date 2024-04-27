8ea6477cf33cc5dfcd0c6f2ee00104be81088983
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2024-04-27 19:02:09 +0430

    Add Tailwind style to the chatcomponent

--- resources/js/components/ChatMessages.vue
+++ resources/js/components/ChatMessages.vue
@@ -2,15 +2,62 @@
 
 <template>
     <div class="left clearfix" v-for="message in messages" :key="message.id">
-            {{ message.user.name }}
-
-            {{ message.message }}
-            <img v-if="message.image" :src="message.image" alt="Message Image" style="width: 400px; height: 400px;">
+        <div class="chat-message">
+            <div
+                :class="{'flex items-end': message.user.id !== user.id, 'flex items-end justify-end': message.user.id == user.id}">
+                <div :class="{
+                        'flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end': message.user.id === user.id,
+                        'flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start': message.user.id !== user.id
+                        }">
+                    <div>
+                        <span :class="{
+                        'px-4 py-2 rounded-lg inline-block bg-blue-600 text-white': message.user.id === user.id,
+                        'px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600': message.user.id !== user.id
+                    }"> {{ message.message }}
+                        </span>
+                    </div>
+                </div>
+                <div :class="{'flex flex-col space-y-2 order-2':  message.user.id === user.id,
+                 'flex flex-col space-y-2 order-1':  message.user.id === user.id }">
+                    <div>
+                        <!--                                <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="w-6 h-6 rounded-full order-1">-->
+                    </div>
+                    <span>{{ message.user.name }}</span>
+                </div>
+            </div>
+        </div>
+        <div>
+            <img v-if="message.image" :src="message.image" alt="Message Image" style="width:100%; height: 400px;">
+        </div>
     </div>
 </template>
+<style>
+.scrollbar-w-2::-webkit-scrollbar {
+    width: 0.25rem;
+    height: 0.25rem;
+}
+
+.scrollbar-track-blue-lighter::-webkit-scrollbar-track {
+    --bg-opacity: 1;
+    background-color: #f7fafc;
+    background-color: rgba(247, 250, 252, var(--bg-opacity));
+}
+
+.scrollbar-thumb-blue::-webkit-scrollbar-thumb {
+    --bg-opacity: 1;
+    background-color: #edf2f7;
+    background-color: rgba(237, 242, 247, var(--bg-opacity));
+}
+
+.scrollbar-thumb-rounded::-webkit-scrollbar-thumb {
+    border-radius: 0.25rem;
+}
+</style>
+
 
 <script>
+
 export default {
-  props: ["messages"],
+    props: ["messages", "user"],
 };
 </script>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,17 +5,38 @@
 @section('content')
     <main class="container">
         <section class="chatroom">
-            <div class="chatroom-header">shoutBox</div>
-            <div class="chatroom-body">
-                <chat-messages :messages="messages"></chat-messages>
+            <div class="chatroom-header">
+                <h1>shoutBox</h1>
+                <div class="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+                    <div class="relative flex items-center space-x-4">
+                        <div class="relative">
+                            <span class="absolute text-green-500 right-0 bottom-0">
+                               <svg width="20" height="20">
+                                  <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
+                               </svg>
+                            </span>
+                            <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="" class="w-10 sm:w-16 h-10 sm:h-16 rounded-full">
+                        </div>
+                        <div class="flex flex-col leading-tight">
+                            <div class="text-2xl mt-1 flex items-center">
+                                <span class="text-gray-700 mr-3">Anderson Vanhron</span>
+                            </div>
+                            <span class="text-lg text-gray-600">Junior Developer</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="messages"  class="chatroom-body flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
+                <chat-messages :messages="messages" :user="{{ Auth::user() }}"></chat-messages>
             </div>
             <div class="chatroom-footer">
                 <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
             </div>
         </section>
-        <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Click me
-        </button>
     </main>
+    <script>
+        const el = document.getElementById('messages')
+        el.scrollTop = el.scrollHeight
+    </script
 @endsection
 
