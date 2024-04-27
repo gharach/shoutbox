859f29fcf8cd0d969d08f06be101f6b2e4f4d1f5
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2024-04-27 21:03:06 +0430

    Craete userlog listener, Register the userlog event in the eventprovider

---
+++ app/Listeners/LogUserLogin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserLoggedIn;
+use App\Models\UserLog;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogUserLogin implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    public function handle(UserLoggedIn $event)
+    {
+        $user = $event->user;
+
+        // Store user log
+        $userLog = new UserLog();
+        $userLog->user_id = $user->id;
+        $userLog->ip_address = request()->ip();
+        $userLog->user_agent = request()->header('User-Agent');
+        $userLog->save();
+    }
+}
