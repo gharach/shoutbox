e06a8949b6700e80c3971b84c9a851ed1c6a66cc
Ali Gharachorloo <ali.gharachorloo@gmail.com>
2024-04-30 19:25:35 +0430

    Add rate limit for sending message requst to 100 per min

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/messages', [App\Http\Controllers\ChatroomController::class, 'getMessages']);
-Route::post('/messages', [App\Http\Controllers\ChatroomController::class, 'sendMessage']);
+Route::post('/messages', [App\Http\Controllers\ChatroomController::class, 'sendMessage'])->middleware('throttle:100,1');
 Route::get('/chat', [App\Http\Controllers\ChatroomController::class, 'index']);
